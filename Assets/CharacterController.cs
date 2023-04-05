a7f73347346d654e8266b39db1fc5359c413c44c
Ambaud Marc <marcambaud@gmail.com>
2023-04-05 09:01:55 +0200

    Script CharacterController

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Variables initialisation
+    public bool move = false;
+    public GameObject sprites;
+    public Rigidbody rb;
+    public float speed = 20f;
+    public bool isgrounded = true;
+    public float rotationspeed = 1f;
+    public float backwardrotspeed= -1f;
+    public bool didihitground = false;
+    public float jumpforce = 10f;
+    public GameObject default;
+    public GameObject Jump_crouch;
+    public GameObject Jump_standing;
+    public float boostspeed;
+    public GameObject losestate;
+    public GameObject loseui;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        default.SetActive(true);
+        Jump_crouch.SetActive(false);
+        Jump_standing.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+        {
+            move = true;
+        }
+        if(Input.GetButtonUp("Fire1") || Input.GetButtonUp("Jump"))
+        {
+            move = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isgrounded)
+        {
+            default.SetActive(true);
+            Jump_crouch.SetActive(false);
+            Jump_standing.SetActive(false);
+        }
+        if(didihitground == false)
+        {
+            rb.AddForce(transform.right * speed * Time.deltaTime*100f, ForceMode.Force);
+        }
+        if(move == true)
+        {
+            if(isgrounded==false)
+            {
+                rb.AddTorque(rotationspeed*rotationspeed, ForceMode.Force);
+                default.SetActive(false);
+                Jump_crouch.SetActive(true);
+                Jump_standing.SetActive(false);
+            }
+            else
+            {
+                rb.AddForce(transform.up*jumpforce*Time.fixedDeltaTime*100f,ForceMode.Force);
+            }
+        }
+        else
+        {
+            if(isgrounded==false)
+            {
+                default.SetActive(false);
+                Jump_crouch.SetActive(false);
+                Jump_standing.SetActive(true);
+            }
+        }
+        if(move == false)
+        {
+            if(isgrounded==false)
+            {
+                rb.AddTorque(backwardrotspeed*1*Time.fixedDeltaTime*100f, ForceMode.Force);
+            }
+        }
+    }
+
+    public void OnCollisionEnter()
+    {
+        isgrounded=true;
+        default.SetActive(true);
+        Jump_crouch.SetActive(false);
+        Jump_standing.SetActive(false);
+    }
+
+    public void OnCollisionExit(){
+        isgrounded = false;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            didihitground = true;
+            losestate.transform.position = gameObject.transform.position;
+            Instansiate(losestate);
+            Destroy(gameObject);
+            cam.SetActive(false);
+            looseui.SetActive(true);
+        }
+        if(collision.tag == "Rock")
+        {
+            didihitground = true;
+            losestate.transform.position = gameObject.transform.position;
+            Instansiate(losestate);
+            Destroy(gameObject);
+            cam.SetActive(false);
+            looseui.SetActive(true);
+        }
+    }
+}
