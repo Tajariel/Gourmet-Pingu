69bd1e4fdf6c72cc725d92b7415e6fdc20f9923c
Ambaud Marc <marcambaud@gmail.com>
2023-04-07 11:15:49 +0200

    Update CharacterGroundFollow.cs

--- Assets/CharacterGroundFollow.cs
+++ Assets/CharacterGroundFollow.cs
@@ -22,7 +22,7 @@ public class CharacterGroundFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.SphereCast(transform.position-transform.right, 0.5f, -(transform.up), out hit, floordistance, lm)) 
+        if (Physics.SphereCast(transform.position, 0.5f, -(transform.up), out hit, floordistance, lm)) 
         {
             Vector3 normal1 = hit.normal;
             if(Physics.SphereCast(transform.position+transform.right, 0.5f, -(transform.up), out hit, floordistance, lm))
@@ -35,7 +35,13 @@ public class CharacterGroundFollow : MonoBehaviour
             {
                 transform.up = normal1;
             }
-            
+            if (Physics.SphereCast(transform.position - transform.right, 0.5f, -(transform.up), out hit, floordistance, lm))
+            {
+                Vector3 normal2 = hit.normal;
+                Vector3 midnormal = (normal1 + normal2) / 2;
+                transform.up = midnormal;
+            }
+
             //transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal));
             if (rb.velocity.magnitude < minimiumSpeed)
             {
